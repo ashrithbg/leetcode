3356a11cc5d9b53b1a562d491fc19e79f3a559c9
Ashrith Bangi <ashrithbg@gmail.com>
2025-10-16 19:32:17 -0700

    Update numberofislands.py

--- graphs/numberofislands.py
+++ graphs/numberofislands.py
@@ -5,15 +5,28 @@ class Solution:
         n = len(grid[0])
         islands = 0
 
+        def getNeighbor(x,y):
+            neighbors = []
+            if x+1<m:
+                neighbors.append((x+1,y))
+            if x-1>=0:
+                neighbors.append((x-1,y))
+            if y+1<n:
+                neighbors.append((x,y+1))
+            if y-1>=0:
+                neighbors.append((x,y-1))
+            return neighbors
+
+
         def bfs(row, col):
-            q = deque([row, col])
+            q = deque([(row, col)])
             while q:
-                (noder,nodec) = q.popleft()
+                noder,nodec = q.popleft()
                 grid[noder][nodec] = '0'
                 for neighborp,neighborq in getNeighbor(noder,nodec):
-                    if grid[neighborp][neighborq] = '1':
+                    if grid[neighborp][neighborq] == '1':
                         grid[neighborp][neighborq] = '0'
-                        q.append([neighborp, neighborq])
+                        q.append((neighborp, neighborq))
 
 
         for row in range(m):
