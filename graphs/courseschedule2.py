3f2c87b79189a10cfc203f02c4d486bb63ad079d
Ashrith Bangi <ashrithbg@gmail.com>
2025-10-19 17:56:05 -0700

    Update courseschedule2.py

--- graphs/courseschedule2.py
+++ graphs/courseschedule2.py
@@ -1,4 +1,10 @@
 class Solution:
+  ## BFS cannot be used to find a cycle in a directed graph
+  ## In DFS we need to find back edges to detect a cycle.
+  ## A backedge can be detected by having a departure time array. 
+  ## If the node is already visited we check if the node has no departure time set. 
+  ## If it is not set that means we have a backedge and a cycle
+  ## Adding an array to save the node before we exit will also give us the order
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adjList = [[] for _ in range(numCourses)]
         visited = [-1]* numCourses
